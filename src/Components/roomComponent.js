7721fdbbe5ebe10f1299b8590ad2221bb7045b81
zarif14-58 <a.jevan14nzs@gmail.com>
2020-07-18 00:44:51 +0600

    Database Creation and Basic Testing

--- src/Components/roomComponent.js
+++ src/Components/roomComponent.js
@@ -1,12 +1,38 @@
 import React, {Component} from 'react'
+import { db } from '../Firebase/config'
 
 
 class Room extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            name: []
+        }
+    }
+
+    async componentDidMount(){
+        db.collection("Rooms").where("public", "==", true)
+            .onSnapshot((querySnapshot) => {
+                let names = []
+                querySnapshot.forEach((doc) => {
+                    names.push(doc.data().roomName)
+                })
+                this.setState({
+                    name: names
+                })
+            })   
+        }
 
     render(){
+        let nms = this.state.name.map(i => {
+            return(
+                <h2>{i}</h2>
+            )
+        })
         return(
             <div>
                 <h1>Rooms</h1>
+                {nms}
             </div>
         )
     }
